e31f91ccc8e6ca5a7806f0bb4cad3856ac40e12f
Bintang Tobing <hello@bintangtobing.com>
2024-11-05 22:37:26 +0700

    update: limitation

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Models\Game;
 use App\Models\Voucher;
 
 Route::get('/', function () {
-    $games = Game::orderBy('created_at', direction: 'DESC')->get();
+    $games = Game::orderBy('created_at', direction: 'DESC')->limit('18')->get();
     return view('pages.index', compact('games'));
 });
 
@@ -18,8 +18,8 @@ Route::get('/contact', function () {
 });
 
 Route::get('/topup', function () {
-    $games = Game::orderBy('created_at', 'DESC')->paginate(12);
-    $vouchers = Voucher::orderBy('created_at', 'DESC')->paginate(12);
+    $games = Game::orderBy('created_at', 'DESC')->paginate(24);
+    $vouchers = Voucher::orderBy('created_at', 'DESC')->paginate(24);
     return view('pages.topup', compact('games', 'vouchers'));
 });
 
